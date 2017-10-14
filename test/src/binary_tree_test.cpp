8dd73ed4054430583df974dbf5bbb5fd478d357d
Eddie Bergman <eddiebergmanhs@gmail.com>
2017-10-14 11:16:11 +0100

    Initial test for lca

---
+++ test/src/binary_tree_test.cpp
@@ -0,0 +1,14 @@
+#include <catch.hpp>
+
+SCENARIO("Lowest Common Ancestor on a binary tree"){
+
+    GIVEN("A populated binary tree"){
+
+        WHEN("lca called on two nodes in the tree"){
+
+            THEN("lca should return their lowest common ancestor"){
+                REQUIRE(1 == 0);
+            }
+        }
+    }
+}
