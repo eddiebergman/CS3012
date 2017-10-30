3bd29e3c20398c41b25bace9b5c8a4c47df00a0b
Eddie Bergman <eddiebergmanhs@gmail.com>
2017-10-30 16:57:50 +0000

    Fixed directed node to now add_child/parent in both directions when adding node

--- src/directed_node.cpp
+++ src/directed_node.cpp
@@ -1,28 +1,39 @@
 #include <directed_node.hpp>
 
-DirectedNode::DirectedNode(int id) : NodeBase(id) {}
+DirectedNode::DirectedNode(int id)
+    : NodeBase(id)
+{
+}
 
 DirectedNode::~DirectedNode() = default;
 
-void DirectedNode::add_child(DirectedNode& n){
+void DirectedNode::add_child(DirectedNode& n)
+{
     children_.insert(&n);
+    n.parents_.insert(this);
 }
 
-void DirectedNode::add_parent(DirectedNode& n){
+void DirectedNode::add_parent(DirectedNode& n)
+{
     parents_.insert(&n);
+    n.children_.insert(this);
 }
 
-bool DirectedNode::has_child(DirectedNode& n){
+bool DirectedNode::has_child(DirectedNode& n)
+{
     return children_.count(&n) == 1;
 }
 
-bool DirectedNode::has_parent(DirectedNode& n){
+bool DirectedNode::has_parent(DirectedNode& n)
+{
     return parents_.count(&n) == 1;
 }
 
-DirectedNode::Container DirectedNode::children(){
+DirectedNode::Container DirectedNode::children()
+{
     return children_;
 }
-DirectedNode::Container DirectedNode::parents(){
+DirectedNode::Container DirectedNode::parents()
+{
     return parents_;
 }
